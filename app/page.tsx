4727f1efd3f7a7262722ca7b10de86bca967475c
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-06-22 00:13:29 +0530

    new project

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="h1">
+        ZynkDrive - The only storage solution you need...</h1>
+      
+    </div>
+  )
+}
